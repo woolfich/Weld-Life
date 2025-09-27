d248e2bb737f40cdb9b0e54bcd00fcde9fd6705c
Woolfich <peresver@gmail.com>
2025-09-27 16:32:30 +0300

    Add import/export functionality

--- service-worker-registration.js
+++ service-worker-registration.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
                 console.log('Service Worker зарегистрирован с областью видимости:', registration.scope);
             })
